bd8e778587b4c90b331e83e24e27069737a27a35
herman <herman.meier@ut.ee>
2023-12-09 15:56:04 +0200

    refactor knn to denoise function

--- Bottom_up (popouts)/Bottom_up (popouts)/denoise.py
+++ Bottom_up (popouts)/Bottom_up (popouts)/denoise.py
@@ -1,18 +1,15 @@
-import cv2
-import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
 
-from position_utils import *
 
-img = cv2.imread('../selection/beaker_01b_13.jpg')
-x, y = find_dot_centres(stimuli_dots(img))
-print(x, y)
+def remove_outliers_knn(points, k=3):
+  knn = NearestNeighbors(n_neighbors=k)
 
-for i in range(len(x)):
-  cv2.circle(img, (int(x[i]), int(y[i])), 1, (0, 0, 255), -1)
+  while True:
+    knn.fit(points.values)
+    connection_counts = knn.kneighbors_graph(points.values).toarray().sum(axis=0)
+    points_to_keep = [points.iloc[i] for i, connections in enumerate(connection_counts) if connections >= 2]
 
-plt.hist(y, bins=40)
-plt.show()
-
-cv2.imshow('final', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    if len(points_to_keep) == len(points):
+      return points_to_keep
+    points = pd.concat(points_to_keep, axis=1).T
--- Bottom_up (popouts)/Bottom_up (popouts)/net.py
+++ Bottom_up (popouts)/Bottom_up (popouts)/net.py
@@ -1,48 +1,65 @@
 import cv2
 import pandas as pd
-from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.neighbors import NearestNeighbors
 
-from position_utils import *
+from position_utils import find_dot_centres, stimuli_dots
+from denoise import remove_outliers_knn
 
 
-def connect_neighbours(image, points):
+def draw_connections(image, points):
   for index, point in points.iterrows():
-    x = point.iloc[0]
-    y = point.iloc[1]
     neighbour_idxs = knn.kneighbors(point.values.reshape(1, -1), return_distance=False)[0]
-    for i in neighbour_idxs:
-      nx = points.iloc[i, 0]
-      ny = points.iloc[i, 1]
-      cv2.line(image, (x, y), (nx, ny), (0, 255), 1)
+    [cv2.line(image, point.values, points.iloc[ni].values, (0, 255), 1) for ni in neighbour_idxs]
+
+
+def draw_filtered_points(image, filtered_points, color=(0, 0, 255)):
+  [cv2.circle(image, point.values, 1, color, -1) for point in filtered_points]
+
 
 
 original = cv2.imread('../selection/beaker_01b_13.jpg')
 xs, ys = find_dot_centres(stimuli_dots(original))
-print(len(xs), len(ys))
 df = pd.DataFrame({'x': xs, 'y': ys}, dtype=int)
-
-# TODO test with radius
 k = 3
-knn = NearestNeighbors(n_neighbors=k)
-knn.fit(df.values)
+# depth = 3
+# knn = NearestNeighbors(n_neighbors=k)
+#
+# cv2.imshow("original", original)
+#
+# for i in range(depth):
+#   print(f"Iteration {i}, points: {len(df)}")
+#   knn.fit(df.values)
+#
+#   connection_graph_img = original.copy()
+#   filtered_points_img = original.copy()
+#
+#   draw_connections(connection_graph_img, df)
+#
+#   connection_matrix = knn.kneighbors_graph(df.values).toarray()
+#   connection_counts = connection_matrix.sum(axis=0)
+#   points_to_keep = [df.iloc[i] for i, connections in enumerate(connection_counts) if connections >= 2]
+#
+#   print(f"Keeping {len(points_to_keep)} points\n")
+#   draw_filtered_points(connection_graph_img, points_to_keep)
+#   draw_filtered_points(filtered_points_img, points_to_keep, color=(150, 200, 0))
+#
+#   df = pd.concat(points_to_keep, axis=1).T
+#
+#   cv2.imshow(f"connected iter {i}", connection_graph_img)
+#   cv2.imshow(f"filtered iter {i}", filtered_points_img)
+#   if cv2.waitKey(0) & 0xFF == ord('q'):
+#     break
+#   else:
+#     continue
 
-connection_graph_img = original.copy()
+# cv2.destroyAllWindows()
+
+
 filtered_points_img = original.copy()
+points = remove_outliers_knn(df, k)
+draw_filtered_points(filtered_points_img, points, color=(150, 200, 0))
+cv2.imshow("original", original)
+cv2.imshow(f"filtered", filtered_points_img)
 
-connect_neighbours(connection_graph_img, df)
-
-connection_matrix = knn.kneighbors_graph(df).toarray()
-[cv2.circle(connection_graph_img, (df.iloc[i, 0], df.iloc[i, 1]), 1, (0, 0, 255), -1) for i, connections in enumerate(connection_matrix.sum(axis=0)) if connections >= 2]
-
-points_to_keep = [df.iloc[i] for i, connections in enumerate(connection_matrix.sum(axis=0)) if connections >= 2]
-print(len(points_to_keep))
-points = pd.concat(points_to_keep, axis=1).T
-[cv2.circle(filtered_points_img, (point.iloc[0], point.iloc[1]), 1, (100, 200, 0), -1) for index, point in points.iterrows()]
-
-
-cv2.imshow('final', original)
-cv2.imshow('connected', connection_graph_img)
-cv2.imshow('filtered', filtered_points_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
